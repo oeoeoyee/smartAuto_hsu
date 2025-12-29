be3421a8fa54e461955923ae75fdcfa100f6053b
oeoeoyee <polly931621@gmail.com>
2025-12-29 22:45:11 +0800

    [feat]初次創建

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+  features: {
+    // Rules for module authors
+    tooling: true,
+    // Rules for formatting
+    stylistic: true,
+  },
+  dirs: {
+    src: [
+      './docs',
+      './pages',
+      './components',
+      './layouts',
+      './assets',
+      './public',
+      './utils',
+      './types',
+      './hooks',
+    ],
+  },
+})
+  .append(
+    // your custom flat config here...
+  )
