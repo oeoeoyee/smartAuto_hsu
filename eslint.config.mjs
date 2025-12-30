da7fa2979c5dc9907481b66e354856c9d35bd428
oeoeoyee <polly931621@gmail.com>
2025-12-30 23:57:38 +0800

    [refactor]修正eslint錯誤

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
   features: {
-    // Rules for module authors
-    tooling: true,
     // Rules for formatting
     stylistic: true,
   },
@@ -23,6 +20,3 @@ export default createConfigForNuxt({
     ],
   },
 })
-  .append(
-    // your custom flat config here...
-  )
